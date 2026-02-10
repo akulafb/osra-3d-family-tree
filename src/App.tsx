4f767e90a1eb72d3aef2f405826a4d11f86e3310
akulafb <akulafb@gmail.com>
2026-02-10 15:50:35 +0300

    Remove ghost reference to deleted PermissionTest component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import InvitePage from './pages/InvitePage';
-import { PermissionTest } from './components/PermissionTest';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/invite/:token" element={<InvitePage />} />
-      <Route path="/debug" element={<PermissionTest />} />
     </Routes>
   );
 }
